1a7c053c8068d169a3bf6ddfa34f92ce1c396bbb
alesoldier <alessandro.comuzzi@studenti.iispascal.it>
2024-06-26 12:38:35 +0200

    refractor: fixes the code layout

--- product-cards.js
+++ product-cards.js
@@ -1,6 +1,6 @@
 const catalogProducts = [
     {
-        altAlert: " it's an ak101 assault rifle",
+        altText: " it's an ak101 assault rifle",
         description: "the ak101 it's a very curious rifle because it combines the look of the ak with the fire power of his historical enemy, chambered in 5.56x45 the ak101 provides one of the most armor piercing experience",
         id: "product-1",
         imageURL: "images/product/ak101-img.jpeg",
@@ -8,7 +8,7 @@ const catalogProducts = [
         price: 299.99
     },
     {
-        altAlert: " it's an ak12 assault rifle",
+        altText: " it's an ak12 assault rifle",
         description: "chose in 2019 for becoming the issued rifle of the russian army the ak12 is a very reliable weapon, thankfully to his adjustable stock and the added rails that can allow the user to utilize ordinary aiming systems has become one of the greatest ak ever invented",
         id: "product-2",
         imageURL: "images/product/ak12-img.jpeg",
@@ -16,7 +16,7 @@ const catalogProducts = [
         price: 399.99
     },
     {
-        altAlert: " it's an ak74 assault rifle",
+        altText: " it's an ak74 assault rifle",
         description: "one of the most famous rifle in the entire world. the ak74 chambered in 5.45x39 combine the strength and the reliability of the 7.62x39 but with his lower diameter provides a better penetration and less stopping power",
         id: "product-3",
         imageURL: "images/product/ak74-img.jpeg",
@@ -24,7 +24,7 @@ const catalogProducts = [
         price: 149.99
     },
     {
-        altAlert: " it's an sr25 DMR",
+        altText: " it's an sr25 DMR",
         description: "precise and accurate the sr25 will never cease to impress, even the most experienced marksman would choose an sr25 over every other rifle. this rifle it's a DMR(designated marksman rifle), cambered in 308 (7.62x51) has an generous stopping power linked with a good penetration against the most advanced body protection",
         id: "product-4",
         imageURL: "images/product/sr25-img.jpeg",
@@ -32,7 +32,7 @@ const catalogProducts = [
         price: 379.99
     },
     {
-        altAlert: " it's an mk17 DMR",
+        altText: " it's an mk17 DMR",
         description: "Long-range rifle with high accuracy and durable construction, chambered in 308 (7.62x51) allow to take down even the most armored target. Thanks to the fire switch we can use the mk17 even in full auto, be careful the recoil kick ass",
         id: "product-5",
         imageURL: "images/product/mk17-img.jpeg",
@@ -40,7 +40,7 @@ const catalogProducts = [
         price: 449.99
     },
     {
-        altAlert: " it's an akm assault rifle",
+        altText: " it's an akm assault rifle",
         description: "the one and only, the most famous rifle, the akm. chambered in 7.62x39 has served in numerous war becoming well known among the people.",
         id: "product-6",
         imageURL: "images/product/akm-img.jpeg",
@@ -110,16 +110,12 @@ function getCatalogProductTemplate(productInfo){
             margin: 0 16px;
         }
 
-        .product-add-button{
-            margin: 0 10%;
-        }
-
         </style>
         <div class="products-container">
             <div class="catalog-product">
 
                 <div class="squared-image-container">
-                    <img class="product-image" src="${productInfo.imageURL}" alt="${productInfo.altAlert}">
+                    <img class="product-image" src="${productInfo.imageURL}" alt="${productInfo.altText}">
                 </div>
 
                 <h5 class="product-title">
@@ -139,8 +135,9 @@ function getCatalogProductTemplate(productInfo){
                         <button class="decrement-quantity-button">-</button>
                         <span class="product-quantity">1</span>
                         <button class="increment-quantity-button">+</button>
-                        <button class="product-add-button" disabled> ADD </button>
                     </span>
+
+                    <button disabled>ADD</button>
                 </div>
             </div>
         </div>`            
